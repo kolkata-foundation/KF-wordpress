5337476d3f6ac71a8578bceeb2115f4a11181fc6
Dhritiman Banerjee <banerjee.dhritiman@gmail.com>
2018-05-15 23:22:13 -0500

    Prettying up the campaign fundraising page

--- page-templates/campaign.php
+++ page-templates/campaign.php
@@ -25,23 +25,34 @@ get_header(); ?>
     $volunteer = $segments[0];
  
     $table_name = $wpdb->prefix . 'fundraisers'; 
-    $result = $wpdb->get_results("SELECT volunteer_names, picture, target, fundraiser_id, campaign_id FROM $table_name WHERE volunteer='" . $volunteer . "'");
+    $result = $wpdb->get_results("SELECT volunteer_names, campaign_pledge, target, fundraiser_id, campaign_id " .
+                                 "FROM $table_name WHERE volunteer='" . $volunteer . "'");
 
     if ($result == null) { # XXX - redirect to the donation page?
     }
 
     $fundraiser_id = $result[0]->fundraiser_id;
+    $goal          = $result[0]->target;
     $table_name  = $wpdb->prefix . 'campaign_donations';
-    $donations   = $wpdb->get_results("SELECT donor_name, donation_amount, is_recurring, donation_date from $table_name where fundraiser_id=" . $fundraiser_id);
+    $donations   = $wpdb->get_results("SELECT donor_name, donation_amount, is_recurring, donation_date " . 
+                                      " FROM $table_name WHERE fundraiser_id=" . $fundraiser_id . 
+                                      " ORDER BY donation_date DESC");
+    $highest_donations = $wpdb->get_results("SELECT donor_name, donation_amount, is_recurring, donation_date " . 
+                                      " FROM $table_name WHERE fundraiser_id=" . $fundraiser_id . 
+                                      " ORDER BY donation_amount DESC");
     $donation_count = $wpdb->num_rows;
+
+    $donation_total = 0; 
+    foreach ($donations as $key => $row) { 
+      $donation_total += $row->donation_amount;
+    }
 ?>
 
     <div class="main-container">
         <div class="row ngo-page-banner">
             <div class="col-lg-10 col-md-10 col-sm-10 col-xs-12 text-center ngo-title">
-                <h1><?php echo $result[0]->volunteer_names; ?> Fundraiser</h1>
-                <h2>Goal $<?php echo $result[0]->target; ?></h2>
-          
+                <h1><?php echo $result[0]->volunteer_names; ?></h1>
+                <h3>Goal: <?php echo $result[0]->campaign_pledge; ?></h3>
             </div>
         </div>        
     </div>
@@ -51,39 +62,59 @@ get_header(); ?>
       <div class="row">
     	<div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 tabbed">
             <?php $project_id = $result[0]->campaign_id; ?>
-            <h1><?php echo get_post($project_id)->post_title; ?></h1>
             <?php echo get_post($project_id)->post_content; ?>
 	</div>
 
         <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 text-center donate-col">
-             <div class="donate-form-container">
-                 
-                 <a href="http://kolkatafoundation.org/donation-form/?fundraiser_id=<?php echo $result[0]->fundraiser_id ?>" class="donate-btn">DONATE</a>
-		 <?php 
-                    $donation_total = 0; 
-                    foreach ($donations as $key => $row) { 
-                       $donation_total += $row->donation_amount;
-                    }
-                 ?>
-                 <div class="indian-donors">Raised $<?php echo $donation_total ?></div>
-                 <div id="my-tab-content" class="tab-content">
-		 <?php foreach ($donations as $key => $row) { ?>
-                   <div class="donate-cause" href="#">
-                     <span class="amount">$
-                        <?php 
-                            echo $row->donation_amount;
-                            if ($row->is_recurring == true) { 
-                                echo "/mo";
-                            } 
-                        ?>
-                     </span>
-                     <span class="cause"><?php echo $row->donor_name ?></span>
-                   </div>
-                   <?php } ?>
-                 </div>
-             </div><!-- donate-form-container -->
-        </div><!-- col-lg-4 -->
-      </div>
-    </div>
+             <div>
+             <a href="http://kolkatafoundation.org/donation-form/?fundraiser_id=<?php echo $result[0]->fundraiser_id ?>" class="donate-btn">
+                 DONATE HERE
+             </a>
+             </div>
+             <div class="campaign-status">
+               <b>$<?php echo $donation_total ?></b> of $<?php echo $goal ?> goal<br/>
+               Raised by <?php echo $donation_count ?> donors
+             </div>
+             <ul class="nav nav-tabs nav-justified">
+                <li class="active"><a data-toggle="tab" href="#recent">Recent</a>
+                <li><a data-toggle="tab" href="#highest">Highest</a></li>
+             </ul>
+             <div class="tab-content">
+                 <div id="recent" class="tab-pane fade in active">
+                 <table class="table table-condensed">
+                 <tbody>
+                 <?php foreach ($donations as $key => $row) { ?>
+                   <tr><td>$
+                      <?php 
+                        echo $row->donation_amount;
+                        if ($row->is_recurring == true) { 
+                          echo "/mo";
+                        } 
+                      ?></td>
+                        <td><?php echo $row->donor_name; ?></td>
+                   </tr>
+                  <?php } ?>
+                 </tbody></table>
+                 </div><!-- tab-pane -->
+                 <div id="highest" class="tab-pane fade">
+                 <table class="table table-condensed">
+                 <tbody>
+                  <?php foreach ($highest_donations as $key => $row) { ?>
+                    <tr><td>$
+                      <?php 
+                        echo $row->donation_amount;
+                        if ($row->is_recurring == true) { 
+                          echo "/mo";
+                        } 
+                      ?></td>
+                       <td><?php echo $row->donor_name; ?></td>
+                    </tr>
+                  <?php } ?>
+                 </tbody></table>
+             </div><!-- tab-panel -->
+        </div><!-- col-lg-3 -->
+      </div><!-- row -->
+    </div><!-- main-container -->
+    <div class="spacer"></div> 
 
 <?php get_footer(); ?>
